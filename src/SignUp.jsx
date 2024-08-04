8c8e4664f2c42d293fd8783e83629f0cf511545d
Siphelele <sphllzulu@gmail.com>
2024-08-04 12:42:56 +0200

    cleaned up my code

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,107 +1,4 @@
-// import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import Avatar from '@mui/material/Avatar';
-// import Button from '@mui/material/Button';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import TextField from '@mui/material/TextField';
-// import { Link } from "react-router-dom";
-// import Grid from '@mui/material/Grid';
-// import Box from '@mui/material/Box';
-// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-// import Typography from '@mui/material/Typography';
-// import Container from '@mui/material/Container';
-// import CircularProgress from '@mui/material/CircularProgress';
-// import axios from 'axios';
 
-// export default function SignUp() {
-//   const navigate = useNavigate();
-//   const [loading, setLoading] = useState(false);
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     setLoading(true);
-//     const data = new FormData(event.currentTarget);
-  
-//     const user = {
-//       email: data.get('email'),
-//       password: data.get('password'),
-//     };
-
-  
-//     axios.post('http://localhost:3001/users', user)
-//       .then(response => {
-//         console.log('User signed up successfully:', response.data);
-//         localStorage.setItem('userId', response.data.id);
-//         localStorage.setItem('userEmail', user.email); 
-//         navigate('/'); 
-//       })
-//       .catch(error => {
-//         console.error('There was an error signing up!', error);
-//       })
-//       .finally(() => {
-//         setLoading(false);
-//       });
-//   };
-
-//   return (
-//     <Container component="main" maxWidth="xs">
-//       <CssBaseline />
-//       <Box
-//         sx={{
-//           marginTop: 8,
-//           display: 'flex',
-//           flexDirection: 'column',
-//           alignItems: 'center',
-//           padding:'3'
-//         }}
-//       >
-//         <Avatar sx={{ m: 1, bgcolor: 'teal' }}>
-//           <LockOutlinedIcon />
-//         </Avatar>
-//         <Typography component="h1" variant="h5">
-//           Sign up
-//         </Typography>
-//         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 3 }}>
-//           <TextField
-//             required
-//             fullWidth
-//             id="email"
-//             label="Email Address"
-//             name="email"
-//             autoComplete="email"
-//             autoFocus
-//             variant='outlined'
-//             sx={{mb:3}}
-//           />
-//           <TextField
-//             required
-//             fullWidth
-//             name="password"
-//             label="Password"
-//             type="password"
-//             id="password"
-//             autoComplete="new-password"
-//             variant='outlined'
-//           />
-//           <Button
-//             type="submit"
-//             fullWidth
-//             variant="outlined"
-//             sx={{ mt: 3, color: 'teal', mb: 2, border: '2px solid teal' }}
-//             disabled={loading}
-//           >
-//             {loading ? <CircularProgress size={24} sx={{ color: 'teal' }} /> : 'Sign Up'}
-//           </Button>
-//           <Grid container>
-//             <Grid item>
-//               <Link style={{ textDecoration: 'none', color: 'teal' }} to="/">Already have an account? Sign In</Link>
-//             </Grid>
-//           </Grid>
-//         </Box>
-//       </Box>
-//     </Container>
-//   );
-// }
 
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -118,7 +15,7 @@ import Container from '@mui/material/Container';
 import CircularProgress from '@mui/material/CircularProgress';
 import axios from 'axios';
 
-// Get the API URL from environment variables
+
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export default function SignUp() {
